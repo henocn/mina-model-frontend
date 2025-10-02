a936041a0229cdd73399afa9a97bf2bc717be485
Henoc N'GASAMA <ngasamah@gmail.com>
2025-10-02 20:34:47 +0000

    Make routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import { useState } from 'react'
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
 
 function App() {
-
   return (
-    <>
-      
-    </>
-  )
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
